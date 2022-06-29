193a27e1a889c552f717af518fe00b3d688d7bb1
danknjoroge <dank8505@gmail.com>
2022-06-29 07:43:18 +0300

    added remove icon function

--- assets/index.js
+++ assets/index.js
@@ -29,15 +29,33 @@ function data(result, word) {
     document.querySelector(".word span").innerText = phon;
     document.querySelector(".meaning span").innerText = def.definition;
     document.querySelector(".synonym span").innerText = syno;
-    document.querySelector(".example span").innerText = def.example;
+    document.querySelector(".example span").innerText = def.example;   
 
+    if (def.synonyms[0] === undefined) {
+      synonyms.parentElement.style.display = "none";
+    } else {
+      synonyms.parentElement.style.display = "block";
+      synonyms.innerHTML = "";
 
-    
+      for(let syn in synonyms){
+        let tag = `<span onclick="search('${definitions.synonyms[syn]}')">${definitions.synonyms[syn]}</span>`;
+        synonyms.insertAdjacentHTML("beforeend",tag );
+
+      }
+    }
   }
 }
-searchInput.addEventListener("keyup", e => {
+searchInput.addEventListener("keyup", (e) => {
   let word = e.target.value.replace(/\s+/g, ' ');
   if (e.key == "Enter" && word) {
     fetchApi(word);
   }
 });
+
+removeIcon.addEventListener("click", ()=>{
+  searchInput.value = "";
+  searchInput.focus()
+  wrapper.classList.remove("active")
+  infoText.textContent.color = "9A9A9A"
+  infoText.innerHTML = 'Type any existing word and press enter to get meaning, example,synonyms, etc.'
+})
